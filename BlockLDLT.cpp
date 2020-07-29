3ab9d558223d718ea1e6df7a37985854ebf8f13b
Tomohiro Suzuki <stomo@yamanashi.ac.jp>
2020-07-29 16:02:58 +0900

    [modify] Output

--- BlockLDLT.cpp
+++ BlockLDLT.cpp
@@ -16,7 +16,7 @@ void Gen_rand_lower_mat(const int m, const int n, double *A)
 
 	for (int j=0; j<n; j++)
 		for (int i=j; i<m; i++)
-				A[i+j*m] = 1.0 - 2*(double)rand() / RAND_MAX;
+				A[i+j*m] = 10.0 - 20.0*(double)rand() / RAND_MAX;
 }
 
 // Show matrix
@@ -30,61 +30,51 @@ void Show_mat(const int m, const int n, double *A)
 	cout << endl;
 }
 
-// Debug mode
-#define DEBUG
-
 int main(const int argc, const char **argv)
 {
 	// Usage "a.out [size of matrix: m ]"
-	assert(argc > 1);
+	if (argc < 2)
+	{
+		cerr << "Usage: a.out [size of matrix]" << endl;
+		return EXIT_FAILURE;
+	}
 
 	const int m = atoi(argv[1]);     // # rows and columns <- the matrix is square
 	const int lda = m;
 
 	double* A = new double [m*m];    // Original matrix
-	int* ipiv = new int[m];          // Pivot vector
-
-	Gen_rand_lower_mat(m,m,A);       // Randomize elements of orig. matrix
-
-	////////// Debug mode //////////
-	#ifdef DEBUG
 	double* OA = new double [m*m];   // Copy of original matrix
-	cblas_dcopy(m*m, A, 1, OA, 1);
-	#endif
-	////////// Debug mode //////////
-
-	double timer = omp_get_wtime();    // Timer start
-
-	assert(0 == LAPACKE_dsytrf(MKL_COL_MAJOR, 'L', m, A, lda, ipiv));
-
-	timer = omp_get_wtime() - timer;   // Timer stop
-
-	cout << m << ", " << timer << ", ";
-
-	////////// Debug mode //////////
-	#ifdef DEBUG
-	// cout << "Debug mode: \n";
+	int* ipiv = new int[m];          // Pivot vector
 
 	double* b = new double [m];      // RHS vector
 	double* x = new double [m];      // Solution vector
+
+	Gen_rand_lower_mat(m,m,A);       // Randomize elements of orig. matrix
+	cblas_dcopy(m*m, A, 1, OA, 1);
+
 	for (int i=0; i<m; i++)
-		b[i] = x[i] = 1.0;
+		x[i] = b[i] = (double)(1.0);
 
+	double timer = omp_get_wtime();    // Timer start
+	assert(0 == LAPACKE_dsytrf(MKL_COL_MAJOR, 'L', m, A, lda, ipiv));
+	timer = omp_get_wtime() - timer;   // Timer stop
+	cout << m << ", " << timer << ", ";
+
+	timer = omp_get_wtime();            // Timer start
 	assert(0 == LAPACKE_dsytrs(LAPACK_COL_MAJOR, 'L', m, 1, A, lda, ipiv, x, lda));
+	timer = omp_get_wtime() - timer;   // Timer stop
+	cout << timer << ", ";
 
+	///////////////////////////////////////////////////////////////////////////////
+	// Check || A*x - b ||
 	cblas_dsymv(CblasColMajor, CblasLower, m, -1.0, OA, lda, x, 1, 1.0, b, 1);
-
-	// cout << "|| b - A*x ||_2 = " << cblas_dnrm2(m, b, 1) << endl;
     cout << cblas_dnrm2(m, b, 1) << endl;
 
+	delete [] A;
 	delete [] OA;
+	delete [] ipiv;
 	delete [] b;
 	delete [] x;
-	#endif
-	////////// Debug mode //////////
-
-	delete [] A;
-	delete [] ipiv;
 
 	return EXIT_SUCCESS;
 }
